98b2dac5a3dd8b31e8853cc19c9a0eb9cb07b5dc
Coder2142 <72675065+Coder2142@users.noreply.github.com>
2021-06-25 20:08:36 -0400

    Update form.js

--- js/form.js
+++ js/form.js
@@ -4,18 +4,20 @@ class Form{
         this.input = createInput("Name");
         this.greeting = createElement("h3");
         this.button = createButton("Play");
+        this.reset = createButton("Reset");
     }
     hide(){
         this.title.hide();
         this.input.hide();
         this.greeting.hide();
         this.button.hide();
+        
     }
         
     display(){
         this.title.html("Space Warfare");
         this.title.position(displayWidth/2-50,40);
-
+        this.reset.position(displayWidth - 100, 50);
         this.button.position(displayWidth/2+30,displayHeight/2);
         this.input.position(displayWidth/2-40,displayHeight/2-80);
 
@@ -32,5 +34,16 @@ class Form{
             this.greeting.html("Hello " + player.name);
             this.greeting.position(displayWidth/2 - 70, displayHeight/4);
         });
+
+        
+        this.reset.mousePressed(()=>{
+            player.updatePlayerCount(0);
+            game.updateGameState(0);
+            //Player.updateCarsAtEnd(0);
+            var playerInfoRef = database.ref('players');
+            playerInfoRef.remove(0);
+          });
     }
+
+
 }
